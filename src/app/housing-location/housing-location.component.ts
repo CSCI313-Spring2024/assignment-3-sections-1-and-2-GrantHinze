dd1b4b9a83e4ea04940a28be84c44449a98af8f2
Grant Hinze <Grant.hinze@ndus.edu>
2025-03-20 19:41:59 -0500

    Added HousingLocation Interface and created a single instance of the new interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-housing-location',
-  imports: [],
-  templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
-})
-export class HousingLocationComponent {
-
-}
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+  }
